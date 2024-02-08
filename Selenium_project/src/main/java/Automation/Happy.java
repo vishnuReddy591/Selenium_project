3ee32b2417dc03c5ed77db3822fc83c23e1e17ea
admin <admin@DESKTOP-AAC0QB5>
2024-02-08 11:15:48 +0530

    initial commit

---
+++ Selenium_project/src/main/java/Automation/Happy.java
@@ -0,0 +1,18 @@
+package Automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Happy {
+	public static void main(String[] args) {
+		try {
+			
+		
+		WebDriver driver = new ChromeDriver();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+	}
+
+}
